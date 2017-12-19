feb661430b25a9d63234e8b16e3df3e380b1a847
whoisnian <zhuchangbao2017@gmail.com>
2017-12-19 10:43:43 +0800

    not addEventListener when no player

--- htdocs/js/player.js
+++ htdocs/js/player.js
@@ -6,19 +6,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var player_time = document.getElementById('player_time');
 	var total_time = document.getElementById('total_time');
 
-	player_button.addEventListener("click", play);
-	player_music.addEventListener("timeupdate", timeUpdate, false);
-	player_slider.addEventListener("click", function(event) {
-		player_music.currentTime = duration * clickPercent(event);
-		player_time.innerHTML = (Math.floor(player_music.currentTime / 60) < 10 ? '0' + Math.floor(player_music.currentTime / 60) : Math.floor(player_music.currentTime / 60)) + ':' + (Math.floor(player_music.currentTime % 60) < 10 ? '0' + Math.floor(player_music.currentTime % 60) : Math.floor(player_music.currentTime % 60));
-	}, false);
+    if(player_button) {
+	    player_button.addEventListener("click", play);
+    }
+	if(player_music) {
+        player_music.addEventListener("timeupdate", timeUpdate, false);
+    }
+    if(player_slider) {
+	    player_slider.addEventListener("click", function(event) {
+		    player_music.currentTime = duration * clickPercent(event);
+		    player_time.innerHTML = (Math.floor(player_music.currentTime / 60) < 10 ? '0' + Math.floor(player_music.currentTime / 60) : Math.floor(player_music.currentTime / 60)) + ':' + (Math.floor(player_music.currentTime % 60) < 10 ? '0' + Math.floor(player_music.currentTime % 60) : Math.floor(player_music.currentTime % 60));
+	    }, false);
+    }
 
 	function clickPercent(event) {
 		return player_slider.value / 100;
 	}
 
-	player_slider.addEventListener('mousedown', mouseDown, false);
-	window.addEventListener('mouseup', mouseUp, false);
+    if(player_slider) {
+	    player_slider.addEventListener('mousedown', mouseDown, false);
+	    window.addEventListener('mouseup', mouseUp, false);
+    }
 
 	var onslider = false;
 
@@ -59,8 +67,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		}
 	}
 
-	player_music.addEventListener("canplaythrough", function() {
-		duration = player_music.duration;
-		total_time.innerHTML = (Math.floor(duration / 60) < 10 ? '0' + Math.floor(duration / 60) : Math.floor(duration / 60)) + ':' + (Math.floor(duration % 60) < 10 ? '0' + Math.floor(duration % 60) : Math.floor(duration % 60));
-	}, false);
+    if(player_music) {
+	    player_music.addEventListener("canplaythrough", function() {
+		    duration = player_music.duration;
+		    total_time.innerHTML = (Math.floor(duration / 60) < 10 ? '0' + Math.floor(duration / 60) : Math.floor(duration / 60)) + ':' + (Math.floor(duration % 60) < 10 ? '0' + Math.floor(duration % 60) : Math.floor(duration % 60));
+	    }, false);
+    }
 });
